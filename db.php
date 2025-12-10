4884f3f9dbf9f23dcd3ca0939353a748835031db
JanisBorovkovs1 <janisdivs@gmail.com>
2025-12-10 14:50:48 +0200

    register php

--- db.php
+++ db.php
@@ -1,5 +1,12 @@
 <?php
-$servername = "";
-$username = "";
+$servername = "localhost";
+$username = "root";
 $password = "";
 $dbname = "auth_demo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
