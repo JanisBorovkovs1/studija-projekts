a04fe3297a0764e3eecf96f42ecc08ee351c83fa
JanisBorovkovs1 <janisdivs@gmail.com>
2025-12-15 12:53:44 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,4 +5,7 @@ session_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = 
+$sql = "SELECT * FROM users WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
