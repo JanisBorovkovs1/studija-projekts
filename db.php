17102a9166461cc2e033c725a5ef3a3b0d86b840
JanisBorovkovs1 <janisdivs@gmail.com>
2026-01-05 12:46:54 +0200

    db izlabots

--- db.php
+++ db.php
@@ -1,13 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "auth_demo";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+$mysqli = mysqli_connect ("localhost","u547027111_mvg","MVGskola1");
+mysqli_select_db ($mysqli,"u547027111_mvg");
+$mysqli->query ("set character_set_client='utf8'");
+$mysqli->query ("set character_set_results='utf8'");
+$mysqli->query ("set collation_connection='utf81_general_ci'");
+$mysqli->query ("SET NAMES utf8");
 ?>
+
