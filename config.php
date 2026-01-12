8be55f17e4e55b6b22b59feafcd0670d26e7f189
JanisBorovkovs1 <janisdivs@gmail.com>
2026-01-12 11:59:24 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ session_start();
 $user = $_POST['login'];
 $parole = $_POST['pass'];
 $parole = SHA1($parole);
+echo  "$user $parole";
 include('db.php');
 $result = $mysqli->query("SELECT * FROM jb_users WHERE username='$user' AND password='$parole'");
 if (mysqli_num_rows($result) > 0){
@@ -10,10 +11,11 @@ if (mysqli_num_rows($result) > 0){
     while ($row = $result->fetch_assoc()){
         $_SESSION['id'] = $row['id_users'];
         }
-    header("Location: next.html");
+    header("Location: next.php");
     }
 else{
-    header("Location: index.html");
+    echo  "nepareizi";
+    //header("Location: index.html");
 }
 
 
