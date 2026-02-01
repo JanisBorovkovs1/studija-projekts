03965be0aeef7eab19de7b915111b6c2aadb003a
JanisBorovkovs1 <janisdivs@gmail.com>
2026-02-01 18:48:04 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,43 +5,24 @@ require 'db.php';
 $email = $_POST['login'] ?? '';
 $password = $_POST['pass'] ?? '';
 
-if (empty($email)) {
-        $error .= '<p class="error">Please enter email.</p>';
-    }
-
-    if (empty($password)) {
-        $error .= '<p class="error">Please enter your password.</p>';
-    }
-
 $sql = "SELECT * FROM jb_users WHERE email = ?";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("s", $email);
 $stmt->execute();
-
 $result = $stmt->get_result();
 
 if ($row = $result->fetch_assoc()) {
-
     if (password_verify($password, $row['password'])) {
-        
+
         $_SESSION['id_users'] = $row['id'];
         $_SESSION['email'] = $row['email'];
 
-        echo '<pre>';
-        var_dump($_SESSION);
-        echo '</pre>';
+        header("Location: next.php");
         exit();
-        //$_SESSION['id_users'] = $row['id'];
-        //$_SESSION['email'] = $row['email'];
-
-        //header("Location: next.php");
-        //exit();
 
     } else {
         echo "Incorrect password";
     }
-
 } else {
     echo "User not found";
 }
-
