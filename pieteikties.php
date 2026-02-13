65038dec1732b2b62cfac60de3df6a26d5ef5b85
JanisBorovkovs1 <janisdivs@gmail.com>
2026-02-13 17:34:19 +0200

    pogu animacijas

--- pieteikties.php
+++ pieteikties.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['id_users'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Īre</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
 </head>
 
 <body class="p-4 bg-light">
