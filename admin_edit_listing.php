d6f5dd16752184f23a0fb3be8be38397118abadb
JanisBorovkovs1 <janisdivs@gmail.com>
2026-03-12 10:52:16 +0200

    Update admin_edit_listing.php

--- admin_edit_listing.php
+++ admin_edit_listing.php
@@ -5,3 +5,41 @@ require 'db.php';
 if ($_SESSION['role'] !== 'admin') {
     die("Access denied.");
 }
+
+$id = $_GET['id'];
+
+$stmt = $mysqli->prepare("SELECT * FROM jb_listings WHERE id_listings=?");
+$stmt->bind_param("i",$id);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+$location = $_POST['location'];
+$contact = $_POST['contact'];
+$price = $_POST['price'];
+
+$stmt = $mysqli->prepare("
+UPDATE jb_listings
+SET location=?, contact=?, price=?
+WHERE id_listings=?
+");
+
+$stmt->bind_param("ssii",$location,$contact,$price,$id);
+$stmt->execute();
+
+header("Location: admin_dashboard.php");
+exit();
+}
+?>
+
+<form method="post">
+
+<input name="location" value="<?= $row['location'] ?>"><br>
+<input name="contact" value="<?= $row['contact'] ?>"><br>
+<input name="price" value="<?= $row['price'] ?>"><br>
+
+<button type="submit">Update</button>
+
+</form>
