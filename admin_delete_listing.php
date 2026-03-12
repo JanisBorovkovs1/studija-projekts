d4e38f37db19992595163e5c2effaae16d6166cb
JanisBorovkovs1 <janisdivs@gmail.com>
2026-03-12 10:41:05 +0200

    Update admin_delete_listing.php

--- admin_delete_listing.php
+++ admin_delete_listing.php
@@ -5,3 +5,13 @@ require 'db.php';
 if ($_SESSION['role'] !== 'admin') {
     die("Access denied.");
 }
+
+$id = $_GET['id'];
+
+$stmt = $mysqli->prepare("DELETE FROM jb_listings WHERE id_listings = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+header("Location: admin_dashboard.php");
+exit();
+?>
